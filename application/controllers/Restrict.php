9e26a6ba01238d7debf86b919c562f8b6bc4c37f
Thiago Amaral <thiago-amaral@msn.com>
2019-03-27 14:04:01 -0300

    ajustando o header e scripts do template

--- application/controllers/Restrict.php
+++ application/controllers/Restrict.php
@@ -5,7 +5,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Restrict extends CI_Controller{
 	
 	public function index(){
-		$this->template->show("login.php");
+		$data = array(
+			"scripts" => array(
+				"util.js",
+				"login.js"
+			)
+		);
+		$this->template->show("login.php", $data);
 	}
 	
 }
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -24,6 +24,12 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+	<?php if (isset($styles)) {
+			foreach ($styles as $style_name) {
+				$href = base_url() . "public/css/" . $style_name; ?>
+				<link href="<?=$href?>" rel="stylesheet">
+			<?php }
+		} ?>
 </head>
 <body id="page-top">
 	<!-- Navigation -->
