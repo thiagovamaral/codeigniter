9e03a9d281984d908cb53fabc07536d89c0cec82
Thiago Amaral <thiago-amaral@msn.com>
2019-03-27 14:25:21 -0300

    Criando funcao ajax_login

--- application/controllers/Restrict.php
+++ application/controllers/Restrict.php
@@ -4,6 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Restrict extends CI_Controller{
 	
+	public function __construct() {
+		parent::__construct();
+		$this->load->library("session");
+	}
+
 	public function index(){
 		$data = array(
 			"scripts" => array(
@@ -14,4 +19,37 @@ class Restrict extends CI_Controller{
 		$this->template->show("login.php", $data);
 	}
 	
+	public function ajax_login() {
+
+		$json = array();
+		$json["status"] = 1;
+		$json["error_list"] = array();
+
+		$username = $this->input->post("username");
+		$password = $this->input->post("password");
+
+		if (empty($username)) {
+			$json["status"] = 0;
+			$json["error_list"]["#username"] = "Usuário não pode ser vazio!";
+		} else {
+			$this->load->model("users_model");
+			$result = $this->users_model->get_user_data($username);
+			if ($result) {
+				$user_id = $result->user_id;
+				$password_hash = $result->password_hash;
+				if (password_verify($password, $password_hash)) {
+					$this->session->set_userdata("user_id", $user_id);
+				} else {
+					$json["status"] = 0;
+				}
+			} else {
+				$json["status"] = 0;
+			}
+			if ($json["status"] == 0) {
+				$json["error_list"]["#btn_login"] = "Usuário e/ou senha incorretos!";
+			}
+		}
+		echo json_encode($json);
+	}
+
 }
