69f16bdde94ea6f429f2db2a0b61238fed2edd4a
Thiago Amaral <thiago-amaral@msn.com>
2019-03-28 12:48:50 -0300

    criando a view restrict

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,12 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->template->show('home');
+		$data = array(
+			"scripts" => array(
+				"owl.carousel.min.js",
+				"theme-scripts.js" 
+			)
+		);
+		$this->template->show("home.php", $data);
 	}
 }
--- application/views/restrict.php
+++ application/views/restrict.php
@@ -1,15 +1,35 @@
 <section style="min-height: calc(100vh - 83px)" class="light-bg">
+
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-offset-3 col-lg-6 text-center">
-				<div class="row">
-					<div class="col-lg-12 text-center">
-						<div class="section-title">
-							<h2>ÁREA RESTRITA</h2>
-						</div>
-					</div>
+				<div class="section-title">
+					<h2>ÁREA RESTRITA</h2>
 				</div>
 			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-offset-5 col-lg-2 text-center">
+				<div class="form-group">
+					<a class="btn btn-link"><i class="fa fa-user"></i></a>
+					<a class="btn btn-link" href="restrict/logoff"><i class="fa fa-sign-out"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+
+		<ul class="nav nav-tabs">
+			<li class="active"><a href="#tab_courses" role="tab" data-toggle="tab">Cursos</a></li>
+			<li><a href="#tab_team" role="tab" data-toggle="tab">Equipe</a></li>
+			<li><a href="#tab_user" role="tab" data-toggle="tab">Usuários</a></li>
+		</ul>
+		<div class="tab-content">
+			<div id="tab_courses" class="tab-pane active">Conteudo curso</div>
+			<div id="tab_team" class="tab-pane">Conteudo equipe</div>
+			<div id="tab_user" class="tab-pane">Conteudo usuarios</div>
 		</div>
 	</div>
 </section>
