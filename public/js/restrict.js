602f4ad663c93880c619ea206db66250fb4fd622
Thiago Amaral <thiago-amaral@msn.com>
2019-04-02 13:05:07 -0300

    Criando a função ajax para adicionar Cursos (JS)

--- public/js/restrict.js
+++ public/js/restrict.js
@@ -2,15 +2,22 @@ $(function() {
 
 	$("#btn_add_course").click(function(){
 		clearErrors();
+		$("#form_course")[0].reset();
+		$("#course_img_path").attr("src", "");
 		$("#modal_course").modal();
 	});
 
 	$("#btn_add_member").click(function(){
-
+		clearErrors();
+		$("#form_member")[0].reset();
+		$("#member_photo_path").attr("src", "");
+		$("#modal_member").modal();
 		$("#modal_member").modal();
 	});
 
 	$("#btn_add_user").click(function(){
+		clearErrors();
+		$("#form_user")[0].reset();
 		$("#modal_user").modal();
 	});
 
@@ -20,5 +27,31 @@ $(function() {
 
 	$("#btn_upload_member_photo").change(function() {
 		uploadImg($(this), $("#member_photo_path"), $("#member_photo"));
+	});
+
+	$("#form_course").submit(function() {
+
+		$.ajax({
+			type: "POST",
+			url: BASE_URL + "restrict/ajax_save_course",
+			dataType: "json",
+			data: $(this).serialize(),
+			beforeSend: function() {
+				clearErrors();
+				$("#btn_save_course").siblings(".help-block").html(loadingImg("Verificando..."));
+			},
+			success: function(response) {
+				clearErrors();
+				if (response["status"]) {
+					$("#modal_course").modal("hide");
+					swal("Sucesso!","Curso salvo com sucesso!", "success");
+					dt_course.ajax.reload();
+				} else {
+					showErrorsModal(response["error_list"])
+				}
+			}
+		})
+
+		return false;
 	});
 })
