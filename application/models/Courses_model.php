03ef23739daa7640f11b6e613a968d6f3f83648a
Thiago Amaral <thiago-amaral@msn.com>
2019-04-02 12:34:46 -0300

    criando os models e seus eventos basicos

---
+++ application/models/Courses_model.php
@@ -0,0 +1,46 @@
+<?php
+
+class Courses_model extends CI_Model {
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->database();
+	}
+
+	public function show_courses() {
+		$this->db->from("courses");
+		return $this->db->get()->result_array();
+	}
+
+	public function get_data($id, $select = NULL) {
+		if (!empty($select)) {
+			$this->db->select($select);
+		}
+		$this->db->from("courses");
+		$this->db->where("course_id", $id);
+		return $this->db->get();
+	}
+
+	public function insert($data) {
+		$this->db->insert("courses", $data);
+	}
+
+	public function update($id, $data) {
+		$this->db->where("course_id", $id);
+		$this->db->update("courses", $data);
+	}
+
+	public function delete($id) {
+		$this->db->where("course_id", $id);
+		$this->db->delete("courses");
+	}
+
+	public function is_duplicated($field, $value, $id = NULL) {
+		if (!empty($id)) {
+			$this->db->where("course_id <>", $id);
+		}
+		$this->db->from("courses");
+		$this->db->where($field, $value);
+		return $this->db->get()->num_rows() > 0;
+	}
+}
