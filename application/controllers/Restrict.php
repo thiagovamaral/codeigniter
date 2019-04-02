96df8b660d1791439c15f7689f7630d0628e1ad5
Thiago Amaral <thiago-amaral@msn.com>
2019-04-02 12:50:42 -0300

    Criando a função ajax_save_course (PHP)

--- application/controllers/Restrict.php
+++ application/controllers/Restrict.php
@@ -100,7 +100,62 @@ class Restrict extends CI_Controller{
 				$json["error"] = "Arquivo não deve ser maior que 1 MB!";
 			}
 		}
+		echo json_encode($json);
+	}
 
+	public function ajax_save_course() {
+
+		if (!$this->input->is_ajax_request()) {
+			exit("Nenhum acesso de script direto permitido!");
+		}
+
+		$json = array();
+		$json["status"] = 1;
+		$json["error_list"] = array();
+
+		$this->load->model("courses_model");
+
+		$data = $this->input->post();
+
+		if (empty($data["course_name"])) {
+			$json["error_list"]["#course_name"] = "Nome do curso é obrigatório!";
+		} else {
+			if ($this->courses_model->is_duplicated("course_name", $data["course_name"], $data["course_id"])) {
+				$json["error_list"]["#course_name"] = "Nome de curso já existente!";
+			}
+		}
+
+		$data["course_duration"] = floatval($data["course_duration"]);
+		if (empty($data["course_duration"])) {
+			$json["error_list"]["#course_duration"] = "Duração do curso é obrigatório!";
+		} else {
+			if (!($data["course_duration"] > 0 && $data["course_duration"] < 100)) {
+				$json["error_list"]["#course_duration"] = "Duração do curso deve ser maior que 0 (h) e menor que 100 (h)!";
+			}
+		}
+
+		if (!empty($json["error_list"])) {
+			$json["status"] = 0;
+		} else {
+			if (!empty($data["course_img"])) {
+				$file_name = basename($data["course_img"]);
+				$old_path = getcwd() . "/tmp/" . $file_name;
+				$new_path = getcwd() . "/public/images/courses/" . $file_name;
+				rename($old_path, $new_path);
+
+				$data["course_img"] = "/public/images/courses/" . $file_name;
+			} else {
+				unset($data["course_img"]);
+			}
+
+			if (empty($data["course_id"])) {
+				$this->courses_model->insert($data);
+			} else {
+				$course_id = $data["course_id"];
+				unset($data["course_id"]);
+				$this->courses_model->update($course_id, $data);
+			}
+		}
 		echo json_encode($json);
 	}
 }
