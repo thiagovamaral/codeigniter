e6ac363a21ae7a07c9a715cff25c056bf2bc06a3
Thiago Amaral <thiago-amaral@msn.com>
2019-04-02 13:30:11 -0300

    Criando a função ajax para adicionar Usuário (JS)

--- public/js/restrict.js
+++ public/js/restrict.js
@@ -80,4 +80,30 @@ $(function() {
 
 		return false;
 	});
+
+	$("#form_user").submit(function() {
+
+		$.ajax({
+			type: "POST",
+			url: BASE_URL + "restrict/ajax_save_user",
+			dataType: "json",
+			data: $(this).serialize(),
+			beforeSend: function() {
+				clearErrors();
+				$("#btn_save_user").siblings(".help-block").html(loadingImg("Verificando..."));
+			},
+			success: function(response) {
+				clearErrors();
+				if (response["status"]) {
+					$("#modal_user").modal("hide");
+					swal("Sucesso!","Usuário salvo com sucesso!", "success");
+					dt_user.ajax.reload();
+				} else {
+					showErrorsModal(response["error_list"])
+				}
+			}
+		})
+
+		return false;
+	});
 })
