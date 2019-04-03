2f285198f27d6a91e89208a1f28ffd0b3c9c880e
Thiago Amaral <thiago-amaral@msn.com>
2019-04-03 12:57:51 -0300

    Criando as funções do DataTable no Model Curso (PHP)

--- application/models/Courses_model.php
+++ application/models/Courses_model.php
@@ -43,4 +43,62 @@ class Courses_model extends CI_Model {
 		$this->db->where($field, $value);
 		return $this->db->get()->num_rows() > 0;
 	}
+
+	var $column_search = array("course_name", "course_description");
+	var $column_order = array("course_name", "", "course_duration");
+
+	private function _get_datatable() {
+		$search = NULL;
+		if ($this->input->post("search")) {
+			$search = $this->input->post("search")["value"];
+		}
+		$order_column = NULL;
+		$order_dir = NULL;
+		$order = $this->input->post("order");
+		if (isset($order)) {
+			$order_column = $order[0]["column"];
+			$order_dir = $order[0]["dir"];
+		}
+
+		$this->db->from("courses");
+		if (isset($search)) {
+			$first = TRUE;
+			foreach ($this->column_search as $field) {
+				if ($first) {
+					$this->db->group_start();
+					$this->db->like($field, $search);
+					$first = FALSE;
+				} else {
+					$this->db->or_like($field, $search);
+				}
+			}
+			if (!$first) {
+				$this->db->group_end();
+			}
+		}
+
+		if (isset($order)) {
+			$this->db->order_by($this->column_order[$order_column], $order_dir);
+		}
+	}
+
+	public function get_datatable() {
+		$length = $this->input->post("length");
+		$start = $this->input->post("start");
+		$this->_get_datatable();
+		if (isset($length) && $length != -1) {
+			$this->db->limit($length, $start);
+		}
+		return $this->db->get()->result();
+	}
+
+	public function records_filtered() {
+		$this->_get_datatable();
+		return $this->db->get()->num_rows();
+	}
+
+	public function records_total() {
+		$this->db->from("courses");
+		return $this->db->count_all_results();
+	}
 }
