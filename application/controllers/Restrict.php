d9984984d62d662f0c0c16b62b31f3126addc28c
Thiago Amaral <thiago-amaral@msn.com>
2019-04-03 12:39:20 -0300

    Instalando e configurando o sweetalert.js e o datatables.js

--- application/controllers/Restrict.php
+++ application/controllers/Restrict.php
@@ -13,7 +13,14 @@ class Restrict extends CI_Controller{
 
 		if ($this->session->userdata("user_id")) {
 			$data = array(
+				"styles" => array(
+					"dataTables.bootstrap.min.css",
+					"datatables.min.css"
+				),
 				"scripts" => array(
+					"sweetalert2.all.min.js",
+					"dataTables.bootstrap.min.js",
+					"datatables.min.js",
 					"util.js",
 					"restrict.js" 
 				),
